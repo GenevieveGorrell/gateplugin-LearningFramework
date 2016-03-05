600b9a0106fd4ff8266033538e1a1ec8da40fe51
Johann Petrak <johann.petrak@gmail.com>
2016-03-05 17:27:13 +0000

    Remove the testing directory.

--- testing/change-type.groovy
+++
@@ -1,5 +0,0 @@
-AnnotationSet mentions = doc.getAnnotations("LearningFramework").get("Mention")
-    for(mention in mentions){
-        FeatureMap fm = mention.getFeatures()
-        fm.put("type", fm.get("LF_class"))
-    }
